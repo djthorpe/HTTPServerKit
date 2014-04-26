35341d92ea2f71b5d9b7f77db001e646130ebf09
David Thorpe <djt@mutablelogic.com>
2014-04-26 14:31:32 +0100

    Updated release script

--- etc/build-release.sh
+++ etc/build-release.sh
@@ -8,6 +8,17 @@ CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONFIGURATION=Release
 PROJECT=${CURRENT_PATH}/../HTTPServerKit.xcodeproj
 
+# determine tag name
+GIT_REVISION_HEAD=`git rev-parse HEAD`
+DATE_REVISION=`date +"%Y%m%d"`
+REVISION="${DATE_REVISION}-${GIT_REVISION_HEAD}"
+echo "REVISION=${REVISION}"
+
+# perform the tagging
+git tag -a -m "Tagging version ${REVISION}" "${REVISION}"
+git push origin --tags
+exit 1
+
 # build mac targets
 xcodebuild -project ${PROJECT} -target "HTTPServerKit" -configuration ${CONFIGURATION} || exit -1
 
